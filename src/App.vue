b63e1fba26bc1201bb3db4065e0865c8969e50d7
Alexander <krasnoyurchenko.a.s@gmail.com>
2018-11-08 18:30:32 +0400

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  div.app#app
+    icon-base(
+    icon-name="vk"
+    )
+      icon-vk
+    img(
+    alt="Vue logo"
+    src="./assets/logo.png"
+    )
+    HelloWorld.__hello(msg="Welcome to Your Vue.js App")
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import IconBase from './components/icons/IconBase';
+  import IconVk from './components/icons/IconVk';
+  import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      IconBase,
+      IconVk,
+      HelloWorld
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  $color: #2c3e50;
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: $color;
+    margin-top: 60px;
+
+    @media (--lt-md) {
+      content: env(--lg);
+    }
+  }
 </style>
