1b076ae202524d1c407305d6a5e91138935badfd
Alexander <krasnoyurchenko.a.s@gmail.com>
2018-11-22 03:07:10 +0400

    edit

--- public/ghspa.js
+++ public/ghspa.js
@@ -24,7 +24,7 @@
 
    /* redirect all 404 trafic to index.html */
    function redirect() {
-     l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + repo + '/?' +
+     l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + repo + '/stage' + '/?' +
               (l.pathname ? 'p=' + l.pathname.replace(/&/g, '~and~').replace(repo, '') : '') +
               (l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
               (l.hash))
