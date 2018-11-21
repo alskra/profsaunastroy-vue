508c685dcc0bbe087fdef58741fa4dafb44d833f
Alexander <krasnoyurchenko.a.s@gmail.com>
2018-11-22 03:12:17 +0400

    edit

--- stage/ghspa.js
+++ stage/ghspa.js
@@ -20,11 +20,11 @@
 
 ;(function(l, projectPages) {
 
-  var repo = projectPages ? '/' + l.pathname.split('/')[1] : ''
+  var repo = projectPages ? '/' + l.pathname.split('/')[1] + '/stage' : ''
 
    /* redirect all 404 trafic to index.html */
    function redirect() {
-     l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + repo + '/stage' + '/?' +
+     l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + repo + '/?' +
               (l.pathname ? 'p=' + l.pathname.replace(/&/g, '~and~').replace(repo, '') : '') +
               (l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
               (l.hash))
